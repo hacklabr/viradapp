8ce106e1233e2bedf1e2d40735a540146743ad2d
Virgílio Santos <virgilio.santos@gmail.com>
2015-05-10 21:07:49 -0300

    project: fix layout and controller for lazyjs

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,36 +1,48 @@
-angular.module('viradapp.controllers', [])
+angular.module('viradapp.controllers',[])
 
-.controller('ProgramacaoCtrl', function($scope, $stateParams, Virada, Lazy) {
-   if($stateParams.atracao){
-       $scope.atracao = Virada.get($stateParams.atracao);
-   } else {
-       Virada.spaces().then(function(data){
-           $scope.spaces = data;
-           Virada.events().then(function(events){
+.controller('ProgramacaoCtrl', function($scope, $stateParams,      Virada, Lazy) {
+    var spaces;
+    $scope.spaces = [];
+
+    var events;
+
+    if($stateParams.atracao){
+        $scope.atracao = Virada.get($stateParams.atracao);
+    } else {
+        Virada.spaces().then(function(data){
+            spaces = Lazy(data);
+            $scope.spaces = spaces.toArray();
+            Virada.events().then(function(events){
                 $scope.events = events;
                 $scope.spaces.forEach(function(space){
                     space.events = Lazy(events).where({
                         spaceId: parseInt(space.id)
-                    }).toArray();
-                    console.log(space.events);
+                    }).chunk(4).toArray();
                 });
-           })
-       });
-   }
+            })
+        });
+    }
+
+    $scope.init = function (e){
+        console.log(e.innerHTML);
+        console.log(this);
+    }
 
 })
 
-.controller('MinhaViradaCtrl', function($scope, Virada, _) {
+.controller('MinhaViradaCtrl', function($scope, Virada, Lazy) {
+    var events;
+    $scope.events = [];
     Virada.events().then(function(data){
-        console.log(data);
-        $scope.events = _.filter(data, function(event){
+        events = Lazy(data);
+        $scope.events = Lazy(events).filter(function(event){
             return event.defaultImageThumb != "";
-        });
+        }).toArray();
     });
 })
 
 .controller('SocialCtrl', function($scope) {
-  $scope.settings = {
-    enableFriends: true
-  };
+    $scope.settings = {
+        enableFriends: true
+    };
 });
