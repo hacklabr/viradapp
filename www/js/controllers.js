a194f1666f0735e147cac7bd529e2949d52b4bd8
Virgílio Santos <virgilio.santos@gmail.com>
2015-05-13 13:59:10 -0300

    programação: fix template and repeat directive
    Here we go back to using ng-repeat instead of colletion repeat.
    RATIONALE: The data we are handling don't need screen fine tunning
    (instead, we can choose) the number of records to show and wait for
    infinite event to load more data.
    
    The changes needed on the template are already made. CSS needed

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -6,27 +6,48 @@ angular.module('viradapp.controllers',[])
 
     var events;
 
+    var loads = 5;
+    var loaded;
+    var page = 1;
+
     if($stateParams.atracao){
         $scope.atracao = Virada.get($stateParams.atracao);
     } else {
         Virada.spaces().then(function(data){
             spaces = Lazy(data);
-            $scope.spaces = spaces.toArray();
-            Virada.events().then(function(events){
-                $scope.events = events;
-                $scope.spaces.forEach(function(space){
-                    space.events = Lazy(events).where({
+            $scope.spaces = spaces.take(loads);
+
+            Virada.events().then(function(data){
+                events = Lazy(data);
+                $scope.spaces = spaces.take(loads)
+                .tap(function(space){
+                    space.events = events.where({
                         spaceId: parseInt(space.id)
-                    }).chunk(4).toArray();
-                });
-            })
+                    }).toArray();
+                }).toArray();
+                loaded = page*loads;
+                console.log("loaded: " + loaded);
+            });
         });
+
     }
 
-    $scope.init = function (e){
-        console.log(e.innerHTML);
-        console.log(this);
-    }
+
+    $scope.loadMore  = function(){
+        setTimeout(function(){
+            page++;
+            var d = spaces.drop(loaded).take(loads)
+            .tap(function(space){
+                space.events = events.where({
+                    spaceId: parseInt(space.id)
+                }).toArray();
+            }).toArray();
+            loaded = page*loads;
+            $scope.spaces.push.apply($scope.spaces, d);
+            $scope.$broadcast('scroll.infiniteScrollComplete');
+            console.log("loaded: " + loaded);
+        }, 1000);
+    };
 
 })
 
