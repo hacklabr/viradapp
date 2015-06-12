6803a7a18f6ef4613a956b9f3219f27d289aff52
Virgílio Santos <virgilio.santos@gmail.com>
2015-06-12 02:53:24 -0300

    facebook: add app_id

--- www/js/config.js
+++ www/js/config.js
@@ -34,7 +34,7 @@ angular.module("viradapp.config", [])
         BASE_URL : "http://viradacultural.prefeitura.sp.gov.br/2015/wp-content/themes/viradacultural-2015/app",
         TEMPLATE_URL : "http://viradacultural.prefeitura.sp.gov.br/2015/wp-content/themes/viradacultural-2015",
         SHARE_URL : "http://viradacultural.prefeitura.sp.gov.br/2015",
-        APP_ID: '1379698002311750'
+        APP_ID: '1460336737533597'
     }
 });
 
