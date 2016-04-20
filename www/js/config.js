be56a2ba487324ef48f0299db0c1547272d8693e
Bruno Martin <bruno.martin@hacklab.com.br>
2016-04-20 03:07:43 -0300

    maloca dragão icons, splash, images and initial layout

--- www/js/config.js
+++ www/js/config.js
@@ -31,10 +31,11 @@ angular.module("viradapp.config", [])
 })
 .factory('GlobalConfiguration', function(){
     return {
-        BASE_URL : "http://viradacultural.prefeitura.sp.gov.br/2015/wp-content/themes/viradacultural-2015/app",
-        TEMPLATE_URL : "http://viradacultural.prefeitura.sp.gov.br/2015/wp-content/themes/viradacultural-2015", // seens deprecated
-        SOCIAL_API_URL : "http://viradacultural.prefeitura.sp.gov.br/2015/api",
-        SHARE_URL : "http://viradacultural.prefeitura.sp.gov.br/2015",
+        APP_NAME: "Maloca Dragão 2016",
+        BASE_URL : "http://maloca.hacklab.com.br/2016/wp-content/themes/maloca/app",
+        TEMPLATE_URL : "http://maloca.hacklab.com.br/2016/wp-content/themes/maloca", // seens deprecated
+        SOCIAL_API_URL : "http://maloca.hacklab.com.br/2016/api",
+        SHARE_URL : "http://maloca.hacklab.com.br/2016",
         APP_ID: '1460336737533597'
     }
 });
