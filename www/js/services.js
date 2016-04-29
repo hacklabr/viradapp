47d6401cedee08ee7f7e37dafa7c60df435c91b4
Bruno Martin <bruno.martin@hacklab.com.br>
2016-04-29 03:14:25 -0300

    simplify services

--- www/js/services.js
+++ www/js/services.js
@@ -2,7 +2,6 @@ angular.module('viradapp.services', [])
 
 .factory('Virada', function($http, GlobalConfiguration, $cordovaFile, $ionicPlatform, MinhaVirada, $q, $cacheFactory) {
     var conf = {
-        assets : "/assets/old/",
         spaces_data : {
             file: "spaces.json"
         },
@@ -18,6 +17,8 @@ angular.module('viradapp.services', [])
     var events = [];
 
     function getSpaces(){
+        // spaces = $http.get(conf.spaces.url);
+
         var $httpCache = $cacheFactory.get('$http');
         var deferred = $q.defer();
 
@@ -51,138 +52,60 @@ angular.module('viradapp.services', [])
         });
     }
 
-    // FIXME Change md5 file get data
-    var data_source = $http.get("assets/new/objects-md5.json")
-    .then(function(newMD5){
-        return $http.get("assets/objects-md5.json").then(function(oldMD5){
-            $ionicPlatform.ready(function(){
-                if(newMD5.data[conf.spaces.file]
-                        !== oldMD5.data[conf.spaces.file]
-                    || newMD5.data[conf.spaces_data.file]
-                        !== oldMD5.data[conf.spaces_data.file]
-                    || newMD5.data[conf.events.file]
-                    !== oldMD5.data[conf.events.file] ){
+    conf.spaces.url = GlobalConfiguration.BASE_URL + "/" + conf.spaces.file;
+    getSpaces();
 
-                        // if(typeof cordova !== 'undefined'){
-                        // $cordovaFile.checkDir(cordova.file.dataDirectory,
-                        //                       "objects1")
-                        // .then(function(data){
-                        // console.log(JSON.stringify(data));
-                        // }, function (error) {
-                        // console.log(JSON.stringify(error));
-                        // });
-                        // }
+    conf.events.url = GlobalConfiguration.BASE_URL + "/" + conf.events.file;
+    getEvents();
 
-                }
-            });
+    conf.spaces_data.url = GlobalConfiguration.BASE_URL + "/" + conf.spaces_data.file;
+    getSpacesData();
 
-
-            conf.spaces.url = newMD5.data[conf.spaces.file] === oldMD5.data[conf.spaces.file] ?
-                conf.assets + conf.spaces.file :
-                GlobalConfiguration.BASE_URL + "/" + conf.spaces.file + "?v=" + newMD5.data[conf.spaces.file];
-            getSpaces();
-
-            conf.events.url = newMD5.data[conf.events.file] === oldMD5.data[conf.events.file] ?
-                conf.assets + conf.events.file :
-                GlobalConfiguration.BASE_URL + "/" + conf.events.file + "?v=" + newMD5.data[conf.events.file];
-            getEvents();
-
-            conf.spaces_data.url = newMD5.data[conf.spaces_data.file] ===
-                oldMD5.data[conf.spaces_data.file] ?
-                conf.assets + conf.spaces_data.file :
-                GlobalConfiguration.BASE_URL + "/" + conf.spaces_data.file + "?v=" + newMD5.data[conf.spaces_data.file];
-            getSpacesData();
-
-            return {spaces : spaces, events: events, spaces_data: spaces_data};
-        }).catch(function(){
-            conf.spaces.url = GlobalConfiguration.BASE_URL + "/" + conf.spaces.file + "?v="
-            + newMD5.data[conf.spaces.file];
-            getSpaces();
-
-            conf.events.url = GlobalConfiguration.BASE_URL + "/" + conf.events.file + "?v="
-            + newMD5.data[conf.events.file];
-            getEvents();
-
-            conf.spaces_data.url = GlobalConfiguration.BASE_URL + "/" + conf.spaces_data.file
-            + "?v=" + newMD5.data[conf.spaces_data.file];
-            getSpacesData();
-
-            return {spaces : spaces, events: events, spaces_data: spaces_data};
-        });
-    }).catch(function(data, status, header, config){
-        // FIXME change md5 file get data
-        return $http.get("assets/objects-md5.json").then(function(oldMD5){
-            conf.spaces.url = conf.assets + conf.spaces.file;
-            getSpaces();
-            conf.events.url = conf.assets + conf.events.file;
-            getEvents();
-            conf.spaces_data.url = conf.assets + conf.spaces_data.file;
-            getSpacesData();
-
-            return {spaces : spaces, events: events, spaces_data: spaces_data};
-        }).catch(function(){
-            console.log("No new file, no old file, first run?");
-            return {spaces: Lazy(spaces), events: Lazy(events), spaces_data: Lazy(spaces_data)};
-        });
-    });
-
+    var data_source = {spaces : spaces, events: events, spaces_data: spaces_data};
 
     return {
         events: function() {
-            return data_source.then(function(data){
-                return data.events;
-            });
+            return data_source.events;
         },
         spaces: function() {
-            return data_source.then(function(data){
-                console.log(data);
-                return data.spaces;
-            });
+            return data_source.spaces;
         },
 
         get: function(event_id) {
-            return data_source.then(function(data){
-                return data.events.then(function(events){
-                    return data.spaces_data.then(function(data){
-                        data = Lazy(data);
-                        var event = events.findWhere({
-                            id : parseInt(event_id)
-                        });
-                        event.space = data.findWhere({
-                            id : parseInt(event.spaceId)
-                        });
-                        return event;
+            return data_source.events.then(function(events){
+                return data_source.spaces_data.then(function(data){
+                    data = Lazy(data);
+                    var event = events.findWhere({
+                        id : parseInt(event_id)
                     });
+                    event.space = data.findWhere({
+                        id : parseInt(event.spaceId)
+                    });
+                    return event;
                 });
             });
         },
         getPalcos: function(){
-            return data_source.then(function(data){
-                return data.spaces_data.then(function(spaces){
-                    return spaces;
-                });
-            });
+            return data_source.spaces_data;
         },
         getPalcoEvents: function(palco_id) {
-            return data_source.then(function(data){
-                return data.spaces_data.then(function(spaces){
-                    var space = spaces.findWhere({
-                        id : parseInt(palco_id)
-                    });
-                    return data.events.then(function(events){
-                        var time = Date.now();
-                        space.events = events.where({
-                            spaceId : parseInt(palco_id)
-                        }).filter(function(ev){
-                            // TODO Show only events in the future
-                            // Here is just the basic idea!
-                            stOn = new Date(ev.startsOn).getTime();
-                            // FIXME Its to be greater than
-                            // return stOn >= time;
-                            return true;
-                        }).toArray();
-                        return space;
-                    });
+            return data_source.spaces_data.then(function(spaces){
+                var space = spaces.findWhere({
+                    id : parseInt(palco_id)
+                });
+                return data_source.events.then(function(events){
+                    var time = Date.now();
+                    space.events = events.where({
+                        spaceId : parseInt(palco_id)
+                    }).filter(function(ev){
+                        // TODO Show only events in the future
+                        // Here is just the basic idea!
+                        stOn = new Date(ev.startsOn).getTime();
+                        // FIXME Its to be greater than
+                        // return stOn >= time;
+                        return true;
+                    }).toArray();
+                    return space;
                 });
             });
         }
